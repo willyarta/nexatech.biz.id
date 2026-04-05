30f5c1bc803694684687372960d0bebb24c3653d
willyarta <willyarta999@gmail.com>
2026-04-06 01:00:25 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const siteConfig = {
   siteName: "HEALTH HUB",
   siteTitle: "Health Insights | Wellness & Longevity",
-  adLink: "LINK IKLAN",
+  adLink: "https://alibisboast.com/qfvmybe5?key=a9f0eca800abf76c8a9a6cab6bc02809",
   domain: "healthhub.biz.id" // This matches your CNAME
 };
 
