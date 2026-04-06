5f40d865f8457f614a7d3b39fcd911cd673d73d6
willyarta <willyarta999@gmail.com>
2026-04-06 13:28:41 +0800

    Delete config.js

--- config.js
+++
@@ -1,8 +0,0 @@
-const siteConfig = {
-  siteName: "HEALTH HUB",
-  siteTitle: "Health Insights | Wellness & Longevity",
-  adLink: "https://alibisboast.com/qfvmybe5?key=a9f0eca800abf76c8a9a6cab6bc02809",
-  domain: "healthhub.biz.id" // This matches your CNAME
-};
-
-export default siteConfig;
